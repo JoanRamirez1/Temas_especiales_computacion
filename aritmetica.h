0bd11c2476d6fd931765f8f8d6554a69e3671104
JoanRamirez1 <joanramirez658@gmail.com>
2026-02-10 11:44:26 -0600

    Se agrego el metodo para multiplicar

--- aritmetica.h
+++ aritmetica.h
@@ -3,3 +3,25 @@ float sumar();
 int sumar(int a, int b) {
     return a + b;
 }
+
+#include <stdio.h>
+
+// Declaración de la función
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1 = 10;
+    int num2 = 5;
+    
+    // Llamada a la función
+    int resultado = sumar(num1, num2);
+    
+    printf("La suma de %d y %d es: %d\n", num1, num2, resultado);
+    
+    return 0;
+}
+int multiplicar(int a, int b) {
+    return a * b;
+}
